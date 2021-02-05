e32e93623b9ab2d46f5261e9376a90280ebbb173
Max <63356200+Flaiers@users.noreply.github.com>
2021-02-05 19:44:12 +0300

    big update

---
+++ cfg.py
@@ -0,0 +1,29 @@
+admins = (id_admins) 
+chats = [''] 
+host = ''  
+userbd = ''  
+passw = ''  
+db = ''  
+domain ="" 
+idoshibki = ""
+proxylogin = ""
+proxypass = ""
+
+help = \
+    "/addproxy type://user:password@0.0.0.0:1234 - добавить прокси\n" \
+    "/delproxy 1 (proxy id) - удалить прокси\n" \
+    "/proxys - список прокси" \
+    "/unban username - разбанить" \
+    "/ban username - забанить"
+proxy = [
+         'socks5://nR6LfaLE2c:ikjegbf484vb9302020@176.107.176.68:37521',
+         'socks5://nR6LfaLE2c:ikjegbf484vb9302020@87.247.154.68:37521',
+         'socks5://nR6LfaLE2c:ikjegbf484vb9302020@193.42.106.0:37521',
+         'socks5://nR6LfaLE2c:ikjegbf484vb9302020@212.90.111.119:37521'
+         ]
+kick_message = ['Пока, ты провинился', 'Тебе тут больше не рады']
+
+
+token = "token"
+
+
